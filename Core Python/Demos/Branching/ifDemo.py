2c9a25348a83698b67ffb649b81421a0128190a9
Yuvraj <yuvrajvarma4215@gmail.com>
2025-08-22 10:46:52 +0530

    august 22

---
+++ Core Python/Demos/Branching/ifDemo.py
@@ -0,0 +1,28 @@
+#### if demo
+
+# num1=float(input('Enter 1st number :: '))
+# num2=float(input('Enter 2nd number :: '))
+
+# if(num1>num2):
+#     print(f'{num1} is greater than {num2}')
+#     print('if block executed.')
+
+# print("End of programm")
+
+num=int(input('Enter number :: '))
+# num2=float(input('Enter 2nd number :: '))
+
+if(num%2 ==0):
+    print(f'{num} is an Even number.')
+else:
+    print(f'{num} is an Odd number.')
+
+
+character=input('Enter character :: ')
+
+
+if( character in 'Yuvraj varma'):
+     print(f'{character} is present in "Yuvraj varma".')
+else:
+         print(f'{character} is Not present in "Yuvraj varma".')
+    
