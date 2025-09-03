905b6f0d040b939c812842f09947cc8869d8ad1f
Yuvraj <yuvrajvarma4215@gmail.com>
2025-09-03 07:58:43 +0530

    assignment 5

--- Core Python/Demos/Patterns/third_pattern.py
+++ Core Python/Demos/Patterns/third_pattern.py
@@ -1,4 +1,24 @@
-for i in range(1,6):
-    for j in range(1,6):
-        print(chr(64+i),end=" ")
+num = 7
+### 1. Increment Pattern
+for i in range(1, num + 1):  # 1 → 7
+    for j in range(i):
+        print("*", end=" ")
     print()
+
+print("-----------------Next pattern----------------")
+### 2. Decrement Pattern
+for i in range(1, num + 1):  # 1 → 7
+    for j in range(num - i + 1):  # decreasing stars
+        print("*", end=" ")
+    print()
+
+print("-----------------Next pattern----------------")
+for i in range(1, 2*num):  # 1 → 13
+    if i <= num:
+        for j in range(i):  # Increasing part
+            print("*", end=" ")
+    
+    else:
+        for j in range(2*num - i):     # Decreasing part
+            print("*", end=" ")
+    print()   
