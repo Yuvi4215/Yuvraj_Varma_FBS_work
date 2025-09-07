b20a2190fc3a9646f6c5625e1e7be12b3432a88d
Yuvraj <yuvrajvarma4215@gmail.com>
2025-09-07 11:57:36 +0530

    7th september

--- Core Python/Assignments/Assignment_5/Question_8.py
+++ Core Python/Assignments/Assignment_5/Question_8.py
@@ -1,10 +1,8 @@
-### Write a program to solve the following series :
-# a. 1! + 2! + 3! + 4! + …..n!
 print("--------------------Start--------------------")
-
+# a. 1! + 2! + 3! + 4! + …..n!
 print("a. 1! + 2! + 3! + 4! + …..n!")
 num = int(input("Enter the number :: "))
-n=num
+n = num
 total = 0
 for i in range(1, n + 1):
     factorial = 1
@@ -18,29 +16,48 @@ print(f"Sum :: {total}")
 # b. N + N^2 + N^3+N^4 …..+N^N (here ^ means exponent)
 print()
 print("b. N + N^2 + N^3+N^4 …..+N^N")
-n=num
-total=0
-for i in range(1,n+1):
-    power=1
-    for j in range(1,i+1):
-        power*=j
-    total+=power
+n = num
+total = 0
+for i in range(1, n + 1):
+    total += n**i  # <-- Corrected to exponent
 print("--------------------Result--------------------")
 print(f"Sum :: {total}")
 
 
 # c. Find the sum of a geometric series from 1 to n where the common ratio is 2.
-# print()
-# print("c. Sum of a geometric series")
-# n=int(input("Enter the number of terms :: "))
-# common_ratio=int(input("Enter Common ratio :: "))
-# total=0
-# while n>0:
-#     common_ratio*=common_ratio
-#     n-=1
-# total=common_ratio-1
-# print("--------------------Result--------------------")
-# print(f"Sum :: {total}")
+print()
+print("c. Sum of a geometric series")
+n = int(input("Enter the number of terms :: "))
+r = 2
+# Formula for geometric series sum: (r^n - 1) / (r - 1)
+total = (r**n - 1) // (r - 1)
+print("--------------------Result--------------------")
+print(f"Sum :: {total}")
 
-# d. S = a + a2 / 2 + a3 / 3 + …… + a10 / 10
-# e. x - x2/3 + x3/5 - x4/7 + …. to n terms
+
+# d. S = a + a^2 / 2 + a^3 / 3 + …… + a^10 / 10
+print()
+print("d. S = a + a^2/2 + a^3/3 + …… + a^10/10")
+a = int(input("Enter the value of a :: "))
+total = 0
+for i in range(1, 11):
+    total += (a**i) / i
+print("--------------------Result--------------------")
+print(f"Sum :: {total}")
+
+
+# e. x - x^2/3 + x^3/5 - x^4/7 + …. to n terms
+print()
+print("e. x - x^2/3 + x^3/5 - x^4/7 + …. to n terms")
+x = int(input("Enter the value of x :: "))
+n = int(input("Enter number of terms :: "))
+total = 0
+sign = 1  # alternates +, -
+denominator = 1
+for i in range(1, n + 1):
+    term = (x**i) / denominator
+    total += sign * term
+    sign *= -1
+    denominator += 2
+print("--------------------Result--------------------")
+print(f"Sum :: {total}")
