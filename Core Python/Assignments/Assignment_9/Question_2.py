f9ce8a5108632ea8956cc105b8b1abfee04e85d6
Yuvraj <yuvrajvarma4215@gmail.com>
2025-09-12 23:48:47 +0530

    final commit

--- Core Python/Assignments/Assignment_9/Question_2.py
+++ Core Python/Assignments/Assignment_9/Question_2.py
@@ -1,29 +1,33 @@
 ### Write a program to check if given number is Armstrong or not using recursive function.
 
+
 def getCount(num):
-    if num==0:
+    if num == 0:
         return 0
-    return 1+ getCount(num//10)
+    return 1 + getCount(num // 10)
 
-def getPower(digit,count):
-    if count==1:
+
+def getPower(digit, count):
+    if count == 1:
         return digit
-    return digit* getPower(digit,count-1)
+    return digit * getPower(digit, count - 1)
 
-def getSum(num,count):
-    if num==0:
+
+def getSum(num, count):
+    if num == 0:
         return 0
-    digit=num%10
-    return getPower(digit, count)+ getSum(num//10,count)
+    digit = num % 10
+    return getPower(digit, count) + getSum(num // 10, count)
+
 
 def isArmstrong(num):
-    count= getCount(num)
+    count = getCount(num)
 
-    if getSum(num,count)==num:
+    if getSum(num, count) == num:
         print(f"{num} is- 'Armstrong number.'")
     else:
         print(f"{num} is- 'NOT Armstrong Number'")
 
 
-num=int(input("Enter the number :: "))
+num = int(input("Enter the number :: "))
 isArmstrong(num)
