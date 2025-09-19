063e51abdc3bd058b91810c68b983929e0750c19
Yuvraj <yuvrajvarma4215@gmail.com>
2025-09-19 19:12:56 +0530

    19th sep commit

--- Core Python/Assignments/Assignment_10/Question_1.py
+++ Core Python/Assignments/Assignment_10/Question_1.py
@@ -1,14 +1,12 @@
 ### Write a program to find sum of all elements of list
-
-li=[10,22,35,69,48,57,25,63,22,"a", 3.1415,85.52]
-
 def getSum(li):
-    sum=0
+    sum = 0
 
     for ele in li:
-        if str(type(ele))=="<class 'int'>" or str(type(ele))=="<class 'float'>":
-            sum+=ele
-
+        if str(type(ele)) == "<class 'int'>" or str(type(ele)) == "<class 'float'>":
+            sum += ele
     return sum
 
 
+li = [10, 22,True, 35, 69, 48, 63, 22, "a", 3.1415, 85.52]
+print(f"Sum of Elements :: {getSum(li)}")
