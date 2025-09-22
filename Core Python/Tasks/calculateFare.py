7bbe5e30317d895fbecdcbce3ff33304c33e94d3
Yuvraj <yuvrajvarma4215@gmail.com>
2025-09-22 22:22:34 +0530

    task done

--- Core Python/Tasks/calculateFare.py
+++ Core Python/Tasks/calculateFare.py
@@ -1,34 +1,36 @@
-### bus total fare calculation based on location and distance
+### bus route and total fare calculation based on location and distance
 
-def getTotalFare(loc,dist,rate):
-    distance, flag=0,True
-    size=len(loc)
-    start=input("Enter Source Location : ")
-    end=input("Enter Destination Location : ")
+
+def getTotalFare(loc, dist, rate):
+    distance, flag = 0, True
+    size = len(loc)
+    start = input("Enter Source Location : ")
+    end = input("Enter Destination Location : ")
     if start in loc and end in loc:
-        for index in range(0,size):
-            if loc[index]==start:
-                j=index
+        for index in range(0, size):
+            if loc[index] == start:
+                j = index
                 while flag:
-                    if loc[j]==end:
-                        flag=False
+                    if loc[j] == end:
+                        flag = False
                     else:
                         # print(dist[j])
-                        distance+=dist[j]
-                        j+=1
-                    if j==size:
-                        j=0
-    return distance*rate
-        
+                        distance += dist[j]
+                        j += 1
+                    if j == size:
+                        j = 0
+    return distance * rate
 
-loc=["s1","s2","s3","s4","s5","s6"]
+
+loc = ["s1", "s2", "s3", "s4", "s5", "s6"]
 print("Stations : ['s1','s2','s3','s4','s5','s6']")
-dist=[1000,2400,3800,1500,2800,4900]
-rate=float(input("Enter the rate(rupess/meter) : "))
+dist = [1000, 2400, 3800, 1500, 2800, 4900]
+rate = float(input("Enter the rate(rupess/meter) : "))
 # getTotalFare(loc,dist,rate)
-total_fare=getTotalFare(loc,dist,rate)
-print(f"""
+total_fare = getTotalFare(loc, dist, rate)
+print(
+    f"""
 ------------------------------------------------      
 Total Fare : {total_fare:.2f} ₹
-------------------------------------------------""")
- 
+------------------------------------------------"""
+)
